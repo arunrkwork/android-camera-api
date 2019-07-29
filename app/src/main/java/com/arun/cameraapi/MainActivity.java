4045d7e6a74c344206ed014838a2118c9381a756
ARUN RAJ KUMAR <arunrkwork@gmail.com>
2019-07-29 15:52:44 +0400

    old camera api

--- app/src/main/java/com/arun/cameraapi/MainActivity.java
+++ app/src/main/java/com/arun/cameraapi/MainActivity.java
@@ -1,14 +1,53 @@
 package com.arun.cameraapi;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCamera;
+    ImageView imgPhoto;
+    public static final int REQUEST_IMAGE_CAPTURE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCamera = findViewById(R.id.btnCamera);
+        imgPhoto = findViewById(R.id.imgPhoto);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCamera();
+            }
+        });
+
+    }
+
+    private void openCamera() {
+        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (camera.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(camera, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle bundle = data.getExtras();
+            Bitmap bitmap = (Bitmap) bundle.get("data");
+            imgPhoto.setImageBitmap(bitmap);
+        }
     }
 }
